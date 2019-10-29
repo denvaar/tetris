493818bb3ac9c6e0b743044079bf0998e9302ce4
Denver Smith <denverpsmith@gmail.com>
2019-10-28 22:16:12 -0600

    Add ghost block (issue #25)

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,6 +2,7 @@ const colors = {
   blue: '\x1b[33m',
   cyan: '\x1b[36m',
   green: '\x1b[32m',
+  darkGray: '\x1b[2;37;40m',
   lightGray: '\x1b[90m',
   lightRed: '\x1b[91m',
   magenta: '\x1b[35m',
